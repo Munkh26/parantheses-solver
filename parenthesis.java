9c676bffe146ee9b2c4554dad34dbfb59c97a4e1
Munkh26 <munkhbatmunkhsoyombo@gmail.com>
2025-11-07 16:00:12 +0000

    creation

---
+++ parenthesis.java
@@ -0,0 +1,9 @@
+// Class author: Munkhsoyombo Munkhbat
+// Date created: 11/07/2025
+// General description:
+
+public class parenthesis {
+    public static void main(String[] args) {
+        
+    }
+}
