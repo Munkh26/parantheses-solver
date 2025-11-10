6d2817344b02383589cf912c7708739c3ee3b199
Munkh26 <munkhbatmunkhsoyombo@gmail.com>
2025-11-10 16:04:27 +0000

    aa

--- parenthesis.java
+++ parenthesis.java
@@ -3,11 +3,27 @@
 // General description:
 
 public class parenthesis {
-    public static valid(String bracket) {
+    public static void valid(String bracket) {
+        int length = bracket.length();
 
+        for (int i = 0; i < length; i++) {
+            String secondChar = bracket.substring(1, 2);
+            String fourthChar = bracket.substring(3, 4);
+
+            if (secondChar.contains("[({")) {
+                if (bracket.substring(i, i + 1).equals(secondChar) || bracket.substring(i, i + 1).equals(fourthChar)) {
+
+                }
+
+            }
+            else {
+                System.out.println("false");;
+                return;
+            }
+        }
     }
     
     public static void main(String[] args) {
-        
+        valid("}");
     }
 }
