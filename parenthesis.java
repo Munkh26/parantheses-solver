633dc5f921592495a06fa73f6cebeec996ca796a
Munkh26 <munkhbatmunkhsoyombo@gmail.com>
2025-11-12 17:58:11 +0000

    big fixes

--- parenthesis.java
+++ parenthesis.java
@@ -5,88 +5,44 @@
 public class parenthesis {
     public static boolean isValid(String bracket) {
         int length = bracket.length();
-        int ind1 = bracket.lastIndexOf("{");
-        int ind2 = bracket.lastIndexOf("[");
-        int ind3 = bracket.lastIndexOf("(");
-        System.out.println(ind1);
-        System.out.println(ind2);
-        System.out.println(ind3);
-        if (length % 2 == 0) {
-            if (bracket.equals("")) {
+        if (bracket == null || length == 0) {
+            return false;
+        }
+        if (length % 2 != 0) {
+            return false;
+        }
+
+        while (bracket.length() > 0) {
+            int ind1 = bracket.lastIndexOf('{');
+            int ind2 = bracket.lastIndexOf('[');
+            int ind3 = bracket.lastIndexOf('(');
+
+            int idx = Math.max(ind1, Math.max(ind2, ind3));
+            if (idx == -1) {
                 return false;
             }
 
-            String start = bracket.substring(0, 1);
-            if (start.contains("}") || start.contains("]") || start.contains(")")) {
+            if (idx + 1 >= bracket.length()) {
                 return false;
             }
 
-    ///fix from here below // need nested loop
-            for (int count = 0; count < length; count++) {
-                String find = bracket.substring(count, count + 1);
-                if (find.contains("{")) {
-                    find = "}";
-                }
-                else if (start.contains("[")) {
-                    find = "]";
-                }
-                else if (start.contains("(")) {
-                    find = ")";
-                }
-                
-                for (int i = 3; i < length; i += 3) {
-                if (find.equals(bracket.substring(i, i + 1))) {
+            char open = bracket.charAt(idx);
+            char close = bracket.charAt(idx + 1);
+            boolean match =
+                (open == '{' && close == '}') ||
+                (open == '[' && close == ']') ||
+                (open == '(' && close == ')');
 
-                }
-                else {
-                    return false;
-                }
-            }
-            }
-            
+            if (!match) return false;
 
-    //Fix from here above
-            if (ind1 > ind2 && ind1 > ind3) {
-                if (ind1 + 2 > length) {
-                    return false;
-                }
-                if (bracket.substring(ind1 + 1, ind1 + 2).equals("}")) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            }
-            else if (ind2 > ind3 && ind2 > ind1) {
-                if (ind2 + 2 > length) {
-                    return false;
-                }
-                if (bracket.substring(ind2 + 1, ind2 + 2).equals("]")) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            }
-            else if (ind3 > ind2 && ind3 > ind1) {
-                if (ind3 + 2 > length) {
-                    return false;
-                }
-                if (bracket.substring(ind3 + 1, ind3 + 2).equals(")")) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            }
-            return false;
+            // remove the matched pair and continue
+            bracket = bracket.substring(0, idx) + bracket.substring(idx + 2);
         }
-        else {
-            return false;
-        }
+
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(isValid("({}){}[]"));
+        System.out.println(isValid(""));
     }
 }
