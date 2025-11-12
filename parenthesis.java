930e20a84224ba9346bd9321c014200af89431c9
Munkh26 <munkhbatmunkhsoyombo@gmail.com>
2025-11-12 16:04:27 +0000

    debug this

--- parenthesis.java
+++ parenthesis.java
@@ -3,7 +3,7 @@
 // General description:
 
 public class parenthesis {
-    public static void valid(String bracket) {
+    public static boolean isValid(String bracket) {
         int length = bracket.length();
         int ind1 = bracket.lastIndexOf("{");
         int ind2 = bracket.lastIndexOf("[");
@@ -11,49 +11,57 @@ public class parenthesis {
         System.out.println(ind1);
         System.out.println(ind2);
         System.out.println(ind3);
+        if (bracket.equals("")) {
+            return false;
+        }
 
+        String start = bracket.substring(0, 1);
+        if (start.contains("}") || start.contains("]") || start.contains(")")) {
+            return false;
+        }
+///fix from here below
+        for (int i = 0; i < length; i += 2) {
+            
+        }
+
+//Fix from here above
         if (ind1 > ind2 && ind1 > ind3) {
             if (ind1 + 2 > length) {
-                System.out.println("false");
-                return;
+                return false;
             }
             if (bracket.substring(ind1 + 1, ind1 + 2).equals("}")) {
-                System.out.println("true");
+                return true;
             }
             else {
-                System.out.println("false");
+                return false;
             }
         }
         else if (ind2 > ind3 && ind2 > ind1) {
             if (ind2 + 2 > length) {
-                System.out.println("false");
-                return;
+                return false;
             }
             if (bracket.substring(ind2 + 1, ind2 + 2).equals("]")) {
-                System.out.println("true");
+                return true;
             }
             else {
-                System.out.println("false");
+                return false;
             }
         }
         else if (ind3 > ind2 && ind3 > ind1) {
             if (ind3 + 2 > length) {
-                System.out.println("false");
-                return;
+                return false;
             }
             if (bracket.substring(ind3 + 1, ind3 + 2).equals(")")) {
-                System.out.println("true");
+                return true;
             }
             else {
-                System.out.println("false");
+                return false;
             }
         }
+        return false;
     }
-            //find last opening bracket
-            //look one to right
 
-    
     public static void main(String[] args) {
-        valid("([{}([)])]");
+        System.out.println(isValid("({)}[]"));
     }
 }
